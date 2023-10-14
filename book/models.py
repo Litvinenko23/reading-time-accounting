6a0aee9f26e2dd64225059d5174fa60fc581404b
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-10-14 13:21:47 +0300

    Created Book & ReadingSession models

--- book/models.py
+++ book/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publication_year = models.PositiveIntegerField()
+    short_description = models.TextField()
+    full_description = models.TextField()
+    last_reading_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class ReadingSession(models.Model):
+    # user = models.ForeignKey()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField()
+
+    def duration(self):
+        return self.end_time - self.start_time
