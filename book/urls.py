836f794932aa1e6026bd62bb51fd659adbdec090
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-10-15 12:37:22 +0300

    Implemented url-router for Book

--- book/urls.py
+++ book/urls.py
@@ -1,1 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from book.views import BookViewSet
+
+router = DefaultRouter()
+router.register("books", BookViewSet)
+
+urlpatterns = [
+    path("", include(router.urls))
+]
+
+app_name = "book"
