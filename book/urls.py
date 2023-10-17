618efa732e8e85a1d0a90b3a82d493bc1b9862d0
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-10-17 11:27:09 +0300

    Installed Celery & Redis

--- book/urls.py
+++ book/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path("start-reading-session/<int:book_id>/", StartReadingSession.as_view(), name="start-reading-session"),
     path("end-reading-session/<int:book_id>/", EndReadingSession.as_view(), name="end-reading-session"),
     path("total-reading-time/<int:book_id>/", TotalReadingTime.as_view(), name="total-reading-time"),
-    path('user-statistics/', UserStatistics.as_view(), name='user-statistics'),
+    path("user-statistics/", UserStatistics.as_view(), name="user-statistics"),
 ]
 
 app_name = "book"
